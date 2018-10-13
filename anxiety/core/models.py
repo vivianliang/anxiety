bed5bab80574f8779021f52e1885355dbc77ccb4
Vivian Liang <vliang88@gmail.com>
2018-10-13 16:11:18 -0700

    Anxiety model

--- anxiety/core/models.py
+++ anxiety/core/models.py
@@ -1,6 +1,11 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class Anxiety(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="anxieties")
+
+    fear = models.TextField(max_length=2048)
+    action = models.TextField(max_length=2048)
+
+    created = models.DateTimeField()
+    updated = models.DateTimeField()
